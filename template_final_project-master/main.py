cc24638afff3ec7614e8ef70272eb8a7b565e18f
Michael DiNapoli <mdinapoli1@binghamton.edu>
2024-12-05 23:54:56 -0500

    commit

--- template_final_project-master/main.py
+++ template_final_project-master/main.py
@@ -6,6 +6,5 @@ def main():
     pygame.init()
     the_controller=Controller()
     the_controller.mainloop()
-# https://codefather.tech/blog/if-name-main-python/
 if __name__ == '__main__':
     main()
