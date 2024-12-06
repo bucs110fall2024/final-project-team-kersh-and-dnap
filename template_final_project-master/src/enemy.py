9934d769cd8c6f7d1749e7fa7a38cb6d48da6492
Will Kershaw <wkershaw@binghamton.edu>
2024-12-05 20:12:42 -0500

    save

--- template_final_project-master/src/enemy.py
+++ template_final_project-master/src/enemy.py
@@ -3,7 +3,7 @@ pygame.init()
 class Enemy(pygame.sprite.Sprite):
     def __init__(self, x, y, enemy, speed=1, img="pngkey.com-gaming-characters-png-1790042.png"):
         super().__init__()
-       # self.image = pygame.image.load(img)
+        self.image = pygame.image.load(img)
         self.image = pygame.Surface((40,40))
         self.image.fill((255, 0, 0))
         self.rect =self.image.get_rect()
