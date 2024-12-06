db6b1ee035ece86162addf265bc9698455462d13
Michael DiNapoli <mdinapoli1@binghamton.edu>
2024-12-05 23:55:53 -0500

    commiting

--- template_final_project-master/src/world.py
+++ template_final_project-master/src/world.py
@@ -38,7 +38,7 @@ class World:
         self.all_sprites.add(ground)
         #ADD FLOATING PLATFORMS
         
-        enemy = Enemy(500, 500, enemy=True, speed=1)
+        enemy = Enemy(500, 450, enemy=True, speed=1)
         self.enemies.add(enemy)
         self.all_sprites.add(enemy)
         self.enemy_spawn_timer = 0
